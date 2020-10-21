5f14360525c787fc493c66d6cc51ed92198c0d4d
Vasha Faris <vashafaris@gmail.com>
2020-10-21 13:26:49 +0700

    add

--- src/app/components/todo-item/todo-item.component.ts
+++ src/app/components/todo-item/todo-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
 
 import { TodoService } from '../../services/todo.service';
 import { Todo } from 'src/app/models/Todo';
@@ -10,6 +10,7 @@ import { Todo } from 'src/app/models/Todo';
 })
 export class TodoItemComponent implements OnInit {
   @Input() todo: Todo;
+  @Output() deleteTodo: EventEmitter<Todo> = new EventEmitter();
 
   constructor(private todoService: TodoService) {}
 
@@ -36,6 +37,6 @@ export class TodoItemComponent implements OnInit {
   }
 
   onDelete(todo: Todo) {
-    console.log('delete');
+    this.deleteTodo.emit(todo);
   }
 }
--- src/app/services/todo.service.ts
+++ src/app/services/todo.service.ts
@@ -24,9 +24,20 @@ export class TodoService {
     return this.http.get<Todo[]>(`${this.todosUrl}${this.todosLimit}`);
   }
 
+  // Add Todo
+  addTodo(todo: Todo): Observable<Todo> {
+    return this.http.post<Todo>(this.todosUrl, todo, httpOptions);
+  }
+
   // Toggle Completed
   toggleCompleted(todo: Todo): Observable<any> {
     const url = `${this.todosUrl}/${todo.id}`;
     return this.http.put(url, todo, httpOptions);
+  }
+
+  // Delete Todo
+  deleteTodo(todo: Todo): Observable<Todo> {
+    const url = `${this.todosUrl}/${todo.id}`;
+    return this.http.delete<Todo>(url, httpOptions);
   }
 }
